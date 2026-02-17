f58a1d041b17280aa09a042b2e3ce31ee8a04d62
a-shirakami <hiroshima1129@outlook.jp>
2026-02-17 09:24:56 +0900

    refactor : generate_tx.py -> main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from io import BytesIO
+from src.my_helper import hash160, SIGHASH_ALL, int_to_little_endian
+from src.my_ecc import PrivateKey
+from src.my_script import Script, p2wpkh_script
+from src.my_tx import Tx, TxIn, TxOut
+from src.bech32 import segwit_encode, segwit_decode
+
+secret = 11292004
+private_key = PrivateKey(secret)
+public_key = private_key.point
+compressed_pubkey = public_key.sec(compressed=True)
+my_pubkey_hash = hash160(compressed_pubkey)
+my_address = segwit_encode("tb", 0, my_pubkey_hash)
+print(my_address)
