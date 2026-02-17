9efc61e1f5f2053bf6c308f10af5d0fe6012e728
a-shirakami <hiroshima1129@outlook.jp>
2026-02-17 09:22:55 +0900

    refactor : main.pyに変更したため削除

--- exapmles/generate_tx.py
+++
@@ -1,6 +0,0 @@
-from io import BytesIO
-from my_helper import hash160, SIGHASH_ALL, int_to_little_endian
-from my_ecc import PrivateKey
-from my_script import Script, p2wpkh_script
-from my_tx import Tx, TxIn, TxOut
-from bech32 import segwit_encode, segwit_decode
